506fc7c33a1bc6e07a1227a655476d82cb193bb5
Shubham Dhumal <shubhamdhumal@Shubhams-MacBook-Pro.local>
2024-03-02 12:42:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,135 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const cookieParser = require('cookie-parser');
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const { User } = require('./model/User');
+const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
+const path = require('path');
+const { env } = require('process');
+
+
+// JWT options
+
+const opts = {};
+opts.jwtFromRequest = cookieExtractor;
+opts.secretOrKey = process.env.JWT_SECRET_KEY; 
+
+//middlewares
+
+server.use(express.static(path.resolve(__dirname, 'build')));
+server.use(cookieParser());
+server.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate('session'));
+server.use(
+  cors({
+    exposedHeaders: ['X-Total-Count'],
+  })
+);
+server.use(express.json()); // to parse req.body
+
+server.use('/users', isAuth(), usersRouter.router);
+server.use('/auth', authRouter.router);
+
+// this line we add to make react router work in case of other routes doesnt match
+server.get('*', (req, res) =>
+  res.sendFile(path.resolve('build', 'index.html'))
+);
+
+// Passport Strategies
+passport.use(
+  'local',
+  new LocalStrategy({ usernameField: 'email' }, async function (
+    email,
+    password,
+    done
+  ) {
+    // by default passport uses username
+    console.log({ email, password });
+    try {
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
+      if (!user) {
+        return done(null, false, { message: 'invalid credentials' }); // for safety
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        'sha256',
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: 'invalid credentials' });
+          }
+          const token = jwt.sign(
+            sanitizeUser(user),
+            process.env.JWT_SECRET_KEY
+          );
+          done(null, { id: user.id, role: user.role, token }); // this lines sends to serializer
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  'jwt',
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user)); // this calls serializer
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log('database connected');
+}
+
+server.listen(process.env.PORT, () => {
+  console.log('server started');
+});
---
+++ services/common.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const nodemailer = require('nodemailer');
+
+
+exports.isAuth = (req, res, done) => {
+  return passport.authenticate('jwt');
+};
+
+exports.sanitizeUser = (user) => {
+  return { id: user.id, role: user.role };
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['jwt'];
+  }
+  return token;
+};
+
